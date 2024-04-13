fad25cc9ad09397d4f7c19852ee9c3e70b0c2926
Gustavo Murillo Vega <gutsmv@gmail.com>
2024-04-13 13:11:36 -0700

    Analysis using k-Fold CV

--- ISLR/05-resampling_k-Fold_CV.R
+++ ISLR/05-resampling_k-Fold_CV.R
@@ -1,13 +1,42 @@
 library(tidyverse)
 library(ISLR2)
 library(MASS)
+library(boot)
 
-set.seed(1)
+# get index of minimal element in a vector
+min_arg <- function(vector) {
+  for (i in 1:length(vector)) {
+    if (CV_error[i] == min(vector)) {
+      min_arg_return <- i
+      break
+    }
+  }
+  min_arg_return
+}
 
 Auto <- Auto |> 
   dplyr::select(-name)
 
-library(boot)
-
+set.seed(1)
 model1 <- glm(mpg ~ ., data = Auto)
 CV1 <- cv.glm(Auto, model1, K = 10)$delta[1]
+
+CV_error <- rep(0, 10)
+for (i in 1:10) {
+  CV_error[i] <- cv.glm(Auto, 
+                        glm(mpg ~ . - horsepower + poly(horsepower, i),
+                            data = Auto),
+                        K = 10)$delta[1]
+}
+
+min(CV_error)
+min_arg(CV_error)
+
+best_poly1 <- glm(mpg ~ . - horsepower + poly(horsepower, min_arg(CV_error)),
+                  data = Auto)
+summary(best_poly1)
+
+best_poly2 <- stepAIC(best_poly1)
+cv.glm(Auto, best_poly1, K = 10)$delta[1]
+cv.glm(Auto, best_poly2, K = 10)$delta[1]
+plot(best_poly2, which = 1)
