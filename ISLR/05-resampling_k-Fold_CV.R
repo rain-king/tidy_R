4422d2bbe5c825fa83d84f689ed1e1e37e1f5e4c
rain-king <gutsmv@gmail.com>
2024-04-22 10:18:21 -0700

    training_test split

--- ISLR/05-resampling_k-Fold_CV.R
+++ ISLR/05-resampling_k-Fold_CV.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(ISLR2)
 library(MASS)
 library(boot)
+source('regression_analysis.R')
 
 # get index of minimal element in a vector
 min_arg <- function(vector) {
@@ -18,14 +19,17 @@ Auto <- Auto |>
   dplyr::select(-name)
 
 set.seed(1)
-model1 <- glm(mpg ~ ., data = Auto)
-CV1 <- cv.glm(Auto, model1, K = 10)$delta[1]
+
+train <- sample(c(T,F), nrow(Auto), replace = T, prob = c(0.8, 0.2))
+
+model1 <- glm(mpg ~ ., data = Auto[train,])
+CV1 <- cv.glm(Auto[train,], model1, K = 10)$delta[1]
 
 CV_error <- rep(0, 10)
 for (i in 1:10) {
-  CV_error[i] <- cv.glm(Auto, 
+  CV_error[i] <- cv.glm(Auto[train,], 
                         glm(mpg ~ . - horsepower + poly(horsepower, i),
-                            data = Auto),
+                            data = Auto[train,]),
                         K = 10)$delta[1]
 }
 
@@ -33,10 +37,11 @@ min(CV_error)
 min_arg(CV_error)
 
 best_poly1 <- glm(mpg ~ . - horsepower + poly(horsepower, min_arg(CV_error)),
-                  data = Auto)
+                  data = Auto[train,])
 summary(best_poly1)
 
 best_poly2 <- stepAIC(best_poly1)
-cv.glm(Auto, best_poly1, K = 10)$delta[1]
-cv.glm(Auto, best_poly2, K = 10)$delta[1]
-plot(best_poly2, which = 1)
+cv.glm(Auto[train,], best_poly1, K = 10)$delta[1]
+cv.glm(Auto[train,], best_poly2, K = 10)$delta[1]
+plot(best_poly1, which = 1)
+R2(Auto$mpg[!train], predict(best_poly1, Auto[!train,]))
