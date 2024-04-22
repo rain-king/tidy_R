d4bd0bce7835c600848da4e8bc77ea327f9781fa
rain-king <gutsmv@gmail.com>
2024-04-21 20:27:48 -0700

    corrected validation set approach

--- ISLR/05-resampling_validation_set.R
+++ ISLR/05-resampling_validation_set.R
@@ -1,36 +1,39 @@
 library(tidyverse)
 library(ISLR2)
 library(MASS)
+source('regression_analysis.R')
 
 set.seed(1)
 
 Auto <- Auto |> 
   dplyr::select(-name)
 
-# validation set
-train_index <- sample(c(T,F), nrow(Auto), replace = T, prob = c(0.8, 0.2))
+# training+validation set index, split 0.7+0.15+0.15 
+train_valid <- sample(c(T,F), nrow(Auto), replace = T, prob = c(0.85, 0.15))
+test = !train_valid
+# training set index 15/85 of 0.85 is 0.15
+train <- sample(c(T,F), sum(train_valid),
+                      replace = T, prob = c(70/85, 15/85))
+validation <- !train
 
-model1 <- lm(mpg ~ ., data = Auto, subset = train_index)
+# train set
+# View(Auto[train_valid,][train,])
+# validation set
+# View(Auto[train_valid,][validation,])
+
+model1 <- lm(mpg ~ ., data = Auto[train_valid,], subset = train)
 summary(model1)
-MSE1 <- (Auto$mpg - predict(model1, Auto))[-train_index]^2 |> mean()
+R_squared1 <- R2(Auto$mpg[train_valid][validation],
+                predict(model1, Auto)[train_valid][validation])
 
 model2 <- stepAIC(model1)
 summary(model2)
-MSE2 <- (Auto$mpg - predict(model2, Auto))[-train_index]^2 |> mean()
+R_squared2 <- R2(Auto$mpg[train_valid][validation],
+                predict(model2, Auto)[train_valid][validation])
 
-model3 <- update(model2, ~ . - cylinders)
+model3 <- update(model2, ~ . - displacement + poly(displacement, 2, raw = T))
 summary(model3)
-MSE3 <- (Auto$mpg - predict(model3, Auto))[-train_index]^2 |> mean()
+R_squared3 <- R2(Auto$mpg[train_valid][validation],
+                predict(model3, Auto)[train_valid][validation])
 
-model4 <- update(model3, ~ . - horsepower)
-summary(model4)
-MSE4 <- (Auto$mpg - predict(model4, Auto))[-train_index]^2 |> mean()
-
-summary(model2)
-model5 <- update(model2, ~ . - displacement + poly(displacement, 2))
-summary(model5)
-MSE5 <- (Auto$mpg - predict(model5, Auto))[-train_index]^2 |> mean()
-
-model6 <- update(model5, ~ . - horsepower + poly(horsepower, 2))
-summary(model6)
-MSE6 <- (Auto$mpg - predict(model6, Auto))[-train_index]^2 |> mean()
+R_squared3_test <- R2(Auto$mpg[test], predict(model3, Auto)[test])
