366f326d3d2e17e0f593ac48646cd5a696ac8913
Gustavo Murillo Vega <gutsmv@gmail.com>
2024-04-23 15:46:53 -0700

    treated NA values and switched the model parameters

--- kaggle_titanic/logistic_regression.R
+++ kaggle_titanic/logistic_regression.R
@@ -14,23 +14,23 @@ test <- test |> dplyr::select(-c(name, cabin, ticket))
 train$age[is.na(train$age)] <- mean(train$age, na.rm = T)
 test$age[is.na(test$age)] <- mean(test$age, na.rm = T)
 
+# train |> filter(if_any(everything(), is.na))
+
 model1 <- glm(survived ~ ., data = train, family = "binomial")
 summary(model1)
-#cv.glm(train, model1, K = 10)$delta[1]
+cv.glm(train |> na.omit(), model1, K = 10)$delta[1]
 
 model2 <- stepAIC(model1) 
 summary(model2)
-#cv.glm(train, model2, K = 10)$delta[1]
+cv.glm(train |> na.omit(), model2, K = 10)$delta[1]
 
 model3 <- update(model2, ~ . - embarked)
 summary(model3)
 cv.glm(train, model3, K = 10)$delta[1]
 
-predict(model1, test, type = 'response')
+predict(model2, test, type = 'response') |> head()
 survived <- rep(0, nrow(test))
-survived[predict(model1, test, type = 'response') > 0.5] <- 1
-mean(survived)
-mean(train$survived)
+survived[predict(model2, test, type = 'response') > 0.5] <- 1
 
 test['survived'] <- survived
 test |>
